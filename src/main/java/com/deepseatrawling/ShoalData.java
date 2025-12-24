a28383a8a76ffd27284c9597ddba5b596ef38847
Maewi <matthebat3@gmail.com>
2025-12-25 09:28:53 +1030

    Added back in some imports that were removed erroneously

--- src/main/java/com/deepseatrawling/ShoalData.java
+++ src/main/java/com/deepseatrawling/ShoalData.java
@@ -1,11 +1,7 @@
 package com.deepseatrawling;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import com.google.common.collect.ImmutableMap;
 import net.runelite.api.GameObject;
 import net.runelite.api.NPC;
 import net.runelite.api.gameval.AnimationID;
